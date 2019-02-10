476713d934f88f072b375a59aadd9eb61b8e7fc0
Denny Fauzan <dennyfauzan92@gmail.com>
2019-02-10 21:37:27 +0700

    deploy

---
+++ dist/precache-manifest.e0eaf1ef2cc6f7d9611aaf590a96445f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "387a0839130d277f8ac1",
+    "url": "/foreign-exchange-currency-app/js/app.fc598831.js"
+  },
+  {
+    "revision": "81ccbc196f9c62ed34df",
+    "url": "/foreign-exchange-currency-app/js/chunk-2d22d746.f878b0d1.js"
+  },
+  {
+    "revision": "98c2dec48ffe3224fc49",
+    "url": "/foreign-exchange-currency-app/css/chunk-42058bfc.927b2caa.css"
+  },
+  {
+    "revision": "98c2dec48ffe3224fc49",
+    "url": "/foreign-exchange-currency-app/js/chunk-42058bfc.f465532a.js"
+  },
+  {
+    "revision": "abb52e1e0d88ac6474eb",
+    "url": "/foreign-exchange-currency-app/css/chunk-vendors.fa219b71.css"
+  },
+  {
+    "revision": "abb52e1e0d88ac6474eb",
+    "url": "/foreign-exchange-currency-app/js/chunk-vendors.34c6c9d8.js"
+  },
+  {
+    "revision": "545457b0abe5b517628e345e649a5338",
+    "url": "/foreign-exchange-currency-app/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/foreign-exchange-currency-app/robots.txt"
+  }
+];
